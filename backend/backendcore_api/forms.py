04bc174993935c25dd0ce4066bdd9bdc72c26535
andreww980 <andreww0717@outlook.com>
2023-04-05 20:24:34 -0400

    FIxed Logo

--- backend/backendcore_api/forms.py
+++ backend/backendcore_api/forms.py
@@ -3,34 +3,35 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
 
-  CUSTOMER = 1
-  CONTRACTOR = 2
+	CUSTOMER = 1
+	CONTRACTOR = 2
 
-  ROLE_CHOICES = (
-		(CUSTOMER, 'Customer'),
-  	(CONTRACTOR, 'Contractor'),
-   )
+	ROLE_CHOICES = (
+			(CUSTOMER, 'Customer'),
+		(CONTRACTOR, 'Contractor'),
+	)
 
-  firstname = forms.CharField(max_length=100, required=False)
-  lastname = forms.CharField(max_length=100, required=False)
-  email = forms.EmailField(required=True)
-  role = forms.ChoiceField(widget=forms.RadioSelect, choices=ROLE_CHOICES)
-class Meta:
-	model = User
-	fields = ("firstname", "lastname", "username", "email", "password1", "password2", "role")
+	first_name = forms.CharField(max_length=30, required=True)
+	last_name = forms.CharField(max_length=30, required=True)
+	email = forms.EmailField(required=True)
+	role = forms.ChoiceField(widget=forms.RadioSelect, choices=ROLE_CHOICES)
 
-def save(self, commit=True):
-	user = super(NewUserForm, self).save(commit=False)
-	user.firstname = self.cleaned_data['firstname']
-	user.lastname = self.cleaned_data['lastname']
-	user.email = self.cleaned_data['email']
-	user.role = self.cleaned_data['role']
+	class Meta:
+		model = User
+		fields = ("first_name", "last_name", "username", "email", "password1", "password2", "role")
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.first_name = self.cleaned_data['first_name']
+		user.last_name = self.cleaned_data['last_name']
+		user.email = self.cleaned_data['email']
+		user.role = self.cleaned_data['role']
 
-	if commit:
-		user.save()
-	return user
+		if commit:
+			user.save()
+		return user
 
