d3b1925b5d9506934c082f09dffe7c589848ab7f
JTechN <97194901+JTechN@users.noreply.github.com>
2023-04-15 16:36:02 -0400

    update to homepage

--- backend/backendcore_api/models.py
+++ backend/backendcore_api/models.py
@@ -17,8 +17,8 @@ class Profile(models.Model):
       img = Image.open(self.image.path) # Open image
 
       # resize image
-      if img.height > 300 or img.width > 300:
-          output_size = (300, 300)
+      if img.height > 200 or img.width > 200:
+          output_size = (200, 200)
           img.thumbnail(output_size) # Resize image
           img.save(self.image.path) # Save it again and override the larger image
 
