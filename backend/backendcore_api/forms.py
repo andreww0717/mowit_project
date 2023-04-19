a29ee82214b783c11184dba42bb04cf6e3b93bc7
JTechN <97194901+JTechN@users.noreply.github.com>
2023-04-19 18:06:51 -0400

    add admin button to admin users

--- backend/backendcore_api/forms.py
+++ backend/backendcore_api/forms.py
@@ -11,22 +11,24 @@ from .models import Profile, UserInfo
 
 class NewUserForm(UserCreationForm):
 
-	group = forms.ModelChoiceField(queryset=Group.objects.all(), required=True, label='Are you a customer or contractor')
-	first_name = forms.CharField(max_length=100, required=True)
-	last_name = forms.CharField(max_length=100, required=True)
-	email = forms.EmailField(required=True)
+    group = forms.ModelChoiceField(queryset=Group.objects.all(), required=True, label='Are you a customer or contractor')
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=10, required=False)
+    address = forms.CharField(max_length=100, required=False)
+    zipcode = forms.CharField(max_length=5, required=False)
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2", "group", "phone_number", "address", "zipcode")
 
-	class Meta:
-		model = User
-		fields = ("first_name", "last_name", "username", "email", "password1", "password2", "group")
 
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
 
-	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
-
-		if commit:
-			user.save()
-		return user
+        if commit:
+            user.save()
+        return user
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
@@ -47,5 +49,5 @@ class UserInfoUpdateForm(forms.ModelForm):
   address = forms.CharField(max_length=100, required=False)
   zipcode = forms.CharField(max_length=5, required=False)
   class Meta:
-        model = UserInfo
+        model = User
         fields = ['phone_number', 'address', 'zipcode']
