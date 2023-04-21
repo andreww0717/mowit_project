a830640f64dcce30755382b97e68806c33c2f9fd
JTechN <97194901+JTechN@users.noreply.github.com>
2023-04-20 20:33:35 -0400

    Clean up coding

--- backend/Customer/models.py
+++ backend/Customer/models.py
@@ -5,29 +5,6 @@ from django.contrib.auth.models import User, AbstractBaseUser, AbstractUser
 
 
 
-# class Customer(AbstractBaseUser):
-#   user = models.OneToOneField(
-#       settings.AUTH_USER_MODEL,
-#       on_delete=models.CASCADE,
-#       null=True
-#   )
-
-#   firstname = models.CharField(max_length=100, null=True)
-#   lastname = models.CharField(max_length=100, null=True)
-#   username = models.CharField(max_length=100, unique=True)
-#   email = models.EmailField(max_length=100, unique=True)
-#   phone = models.CharField(max_length=10, null=True)
-#   address = models.CharField(max_length=200, null=True)
-#   zipcode = models.CharField(max_length=10, null=True)
-#   #date_created = models.DateField(auto_now_add=True, null=True)
-
-#   is_staff = models.BooleanField(default=True)
-#   is_active = models.BooleanField(default=True)
-#   role = models.CharField(max_length=100, blank=True, null=True)
-
-#   USERNAME_FIELD = 'username'
-
-
 
 #Rating System for Customer
 
